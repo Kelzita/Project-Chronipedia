7f92aca95d6ba0c575b94ea05fba82690bd8e48c
raquelfernandes <raquelfernandesbriyoidjdj@gmail.com>
2025-06-15 00:14:15 -0300

    Update conexao.php

--- PHP/conexao.php
+++ PHP/conexao.php
@@ -2,7 +2,7 @@
 $host = 'localhost';
 $db   = 'chronipedia';
 $user = 'root';
-$pass = '1234'; // Se você entrou no phpMyAdmin sem senha, deixe vazio
+$pass = '1234'; // isso aqui deu dor de cabeça 
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
